23c2289f143ea3bc775e9019d22c0c927ea58aea
Mathieu <studio.creatic@gmail.com>
2022-04-11 11:44:12 +0200

    suppression fixture

--- src/Entity/Room.php
+++ src/Entity/Room.php
@@ -28,7 +28,25 @@ class Room
     private $pictures;
 
     #[ORM\Column(type: 'integer')]
+    private $capacity;
+
+    #[ORM\Column(type: 'integer')]
+    private $size;
+
+    #[ORM\Column(type: 'integer')]
+    private $bed;
+
+    #[ORM\Column(type: 'integer')]
     private $price;
+
+    #[ORM\Column(type: 'integer')]
+    private $quantity_rooms;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $breaksfast;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $header_room;
 
     #[ORM\ManyToOne(targetEntity: Hotel::class, inversedBy: 'rooms')]
     #[ORM\JoinColumn(nullable: false)]
@@ -87,6 +105,42 @@ class Room
         return $this;
     }
 
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): self
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getBed(): ?int
+    {
+        return $this->bed;
+    }
+
+    public function setBed(int $bed): self
+    {
+        $this->bed = $bed;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
     public function getPrice(): ?int
     {
         return $this->price;
@@ -95,6 +149,42 @@ class Room
     public function setPrice(int $price): self
     {
         $this->price = $price;
+
+        return $this;
+    }
+
+    public function getQuantityRooms(): ?int
+    {
+        return $this->quantity_rooms;
+    }
+
+    public function setQuantityRooms(int $quantity_rooms): self
+    {
+        $this->quantity_rooms = $quantity_rooms;
+
+        return $this;
+    }
+
+    public function getBreaksfast(): ?string
+    {
+        return $this->breaksfast;
+    }
+
+    public function setBreaksfast(string $breaksfast): self
+    {
+        $this->breaksfast = $breaksfast;
+
+        return $this;
+    }
+
+    public function getHeaderRoom(): ?string
+    {
+        return $this->header_room;
+    }
+
+    public function setHeaderRoom(?string $header_room): self
+    {
+        $this->header_room = $header_room;
 
         return $this;
     }
