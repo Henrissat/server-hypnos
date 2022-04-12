3a45c60b14900015e67f5d76e453bfe67be715fb
Mathieu <studio.creatic@gmail.com>
2022-04-12 10:39:25 +0200

    nouvelle migration

---
+++ migrations/Version20220412082939.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220412082939 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE galery_img (id INT AUTO_INCREMENT NOT NULL, rooms_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_A0A614108E2368AB (rooms_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE gerant (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, firstname VARCHAR(255) , lastname VARCHAR(255) , picture VARCHAR(255), id_hotel INT , password VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_D1D45C70E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE hotel (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, city VARCHAR(50) NOT NULL, adress VARCHAR(255) DEFAULT NULL, title VARCHAR(100) DEFAULT NULL, description LONGTEXT DEFAULT NULL, img_header VARCHAR(255) DEFAULT NULL, vignette VARCHAR(255) DEFAULT NULL, stars INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE room (id INT AUTO_INCREMENT NOT NULL, hotel_id INT NOT NULL, name VARCHAR(50) NOT NULL, shortdescription VARCHAR(100) DEFAULT NULL, description LONGTEXT DEFAULT NULL, pictures VARCHAR(255) DEFAULT NULL, capacity INT NOT NULL, size INT, bed INT NOT NULL, price INT NOT NULL, quantity_rooms INT NOT NULL, breaksfast VARCHAR(50), header_room VARCHAR(255) DEFAULT NULL, INDEX IDX_729F519B3243BB18 (hotel_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, adress VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE galery_img ADD CONSTRAINT FK_A0A614108E2368AB FOREIGN KEY (rooms_id) REFERENCES room (id)');
+        $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519B3243BB18 FOREIGN KEY (hotel_id) REFERENCES hotel (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519B3243BB18');
+        $this->addSql('ALTER TABLE galery_img DROP FOREIGN KEY FK_A0A614108E2368AB');
+        $this->addSql('DROP TABLE galery_img');
+        $this->addSql('DROP TABLE gerant');
+        $this->addSql('DROP TABLE hotel');
+        $this->addSql('DROP TABLE room');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
