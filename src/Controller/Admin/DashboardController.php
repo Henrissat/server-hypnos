e0d6ecb2cf5ea6a684e0971b3cb8de6155b29cc5
Mathieu <studio.creatic@gmail.com>
2022-05-10 11:48:17 +0200

    modif fichier .env

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -26,33 +26,13 @@ class DashboardController extends AbstractDashboardController
     //#[IsGranted('ROLE_ADMIN')]
     #[Route('/admin', name: 'admin')]
     public function index(): Response
-    {
-
-        //return parent::index();
-        
-        //return $this->render('admin/dashboard.html.twig');
-        
+    {       
         //créer la rediction vers les CRUD entity déjà créées
         $url = $this->adminUrlGenerator
             ->setController(RoomCrudController::class)
             ->generateUrl();
         return $this->redirect($url);
 
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        /*$adminUrlGenerator = $this->container->get(RoomCrudController::class);
-        return $this->redirect($adminUrlGenerator->setController(RoomCrudController::class)->generateUrl());*/
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
